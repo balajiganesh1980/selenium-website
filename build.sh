a2ae73e885fdbd0e771d339aa871ae0415a0107d
balajiganesh1980 <balaji1980.ganesh@outlook.com>
2020-06-10 02:06:27 +0530

    commit jar file

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo docker build ./mysql/ -t db
-sudo docker run -it -d -p 3306:3306 --name mysql db
+sudo docker run -it -d -p 3307:3307 --name mysql db
 
 sudo docker build ./website/ -t web
-sudo docker run -it -d -p 3001:80 --link mysql:db web
+sudo docker run -it -d -p 3001:3001 --link mysql:db web
